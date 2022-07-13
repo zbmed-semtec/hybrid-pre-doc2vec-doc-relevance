72519980b25b5e11db7b1a106d515e0151a91865
guillermo1996 <guillermorocamora@gmail.com>
2022-07-13 10:15:28 +0200

    Added preprocessing and model functions

---
+++ code/embeddings/create_model.py
@@ -0,0 +1,244 @@
+import sys
+import time
+
+import logging
+import argparse
+import numpy as np
+import pandas as pd
+
+from typing import Tuple, List
+from gensim.models.doc2vec import Doc2Vec, TaggedDocument
+
+
+def load_numpy_file(input_path: str) -> Tuple[List[int], List[List[str]]]:
+    """
+    Loads a .NPY file with the preprocessed articles. The files should be
+    generated using the preprocess script.
+
+    Parameters
+    ----------
+    input_path : str
+        Input path for the .NPY file.
+
+    Returns
+    -------
+    pmid: List[int]
+        The pmid of the articles in a list.
+    join_text: List[str]
+        The combination of title + abstract of the articles in a list.
+    """
+    data = np.load(input_path, allow_pickle=True)
+
+    pmid = [doc[0].item() for doc in data]
+    #titles = [i[1].tolist() for i in a]
+    #abstract = [i[2].tolist() for i in a]
+    join_text = [doc[1].tolist() + doc[2].tolist() for doc in data]
+
+    return pmid, join_text
+
+
+def load_tsv_file(input_path: str) -> Tuple[List[int], List[List[str]]]:
+    """
+    Loads a .TSV file with the preprocessed articles. The files should be
+    generated using the preprocess script, or any .TSV with three columns:
+    PMID, title and abstract.
+
+    Parameters
+    ----------
+    input_path : str
+        Input path for the .TSV file.
+
+    Returns
+    -------
+    pmid: List[int]
+        The pmid of the articles in a list.
+    join_text: List[str]
+        The combination of title + abstract of the articles in a list.
+    """
+    data = pd.read_csv(input_path, delimiter="\t", quotechar="`")
+
+    pmid = list(data["PMID"])
+    #title = [title.split() for title in data["title"]]
+    #abstract = [abstract.split() for abstract in data["abstract"]]
+    join_text = [doc.split()
+                 for doc in (data["title"] + " " + data["abstract"])]
+
+    return pmid, join_text
+
+
+def load_tokens(input_path: str) -> Tuple[List[int], List[List[str]]]:
+    """
+    Read from either a .TSV or a .NPY file the data.
+
+    Parameters
+    ----------
+    input_path : str
+        Input path for the file.
+
+    Returns
+    -------
+    Tuple[List[int], List[List[str]]]
+        The output from any of load_tsv_file() or load_numpy_file()
+    """
+    if input_path.endswith(".tsv"):
+        return load_tsv_file(input_path)
+    elif input_path.endswith(".npy"):
+        return load_numpy_file(input_path)
+    else:
+        logging.error("Input must be a .TSV or .NPY file.")
+        sys.exit("Unrecognize input file extension.")
+
+
+def generate_TaggedDocument(pmid: List[int], join_text: List[List[str]]) -> TaggedDocument:
+    """
+    Generates the tagged documents required for Doc2Vec.
+
+    Parameters
+    ----------
+    pmid : List[int]
+        List of PMIDs extracted from the data.
+    join_text : List[List[str]]
+        List containing to tokens from each article. Title and abstract are
+        combined in one text by default.
+
+    Returns
+    -------
+    tagged_data: TaggedDocument
+        TaggedDocument where every PMID is tagged into an element of the token
+        list.
+    """
+    tagged_dict = dict(zip(pmid, join_text))
+    tagged_data = [TaggedDocument(words=value, tags=[str(key)])
+                   for key, value in tagged_dict.items()]
+
+    return tagged_data
+
+
+def generate_doc2vec_model(
+    tagged_data: TaggedDocument, 
+    params: dict = {
+        "vector_size": 200, 
+        "window": 5, 
+        "min_count": 1, 
+        "epochs": 5, 
+        "workers": 4}
+    ) -> Doc2Vec:
+    """
+    Generates de Doc2Vec model and builds the vocabulary from the
+    taggedDocuments.
+
+    Parameters
+    ----------
+    tagged_data : TaggedDocument
+        TaggedDocument where every PMID is tagged into an element of the token
+        list.
+    params : dict, optional
+        Dictionary containing the parameters to create the Doc2Vec model, by
+        default { "vector_size": 200, "window": 5, "min_count": 1, "epochs": 5,
+        "workers": 4}
+
+    Returns
+    -------
+    model: Doc2Vec
+        Doc2Vec model.
+    """
+
+    #model = Doc2Vec(vector_size=200, window=5, min_count=1, epochs=5, workers = 4)
+    model = Doc2Vec(**params)
+    model.build_vocab(tagged_data)
+
+    return model
+
+
+def train_doc2vec_model(model: Doc2Vec, tagged_data: TaggedDocument, time_train: bool = False) -> None:
+    """
+    Trains the model using the taggedDocuments. Since the model had the
+    vocabulary built already, we only need to call for the train() function.
+
+    Parameters
+    ----------
+    model : Doc2Vec
+        Doc2Vec model.
+    tagged_data : TaggedDocument
+        TaggedDocument where every PMID is tagged into an element of the token
+        list.
+    """
+    if time_train: start_time = time.time()
+
+    model.train(tagged_data, total_examples=model.corpus_count,
+                epochs=model.epochs)
+
+    if time_train: print("--- Time to train: {:.2f} seconds".format(time.time() - start_time))
+
+def save_doc2vec_model(model: Doc2Vec, output_path: str) -> None:
+    """
+    Saves the Doc2Vec model into the desired path.
+
+    Parameters
+    ----------
+    model : Doc2Vec
+        Doc2Vec model.
+    output_path : str
+        Path for output model.
+    """
+    model.save(output_path)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    #group = parser.add_mutually_exclusive_group(required=True)
+    parser.add_argument("-i", "--input", type=str,
+                        help="Path to input .NPY or .TSV file", required=True)
+    parser.add_argument("-o", "--output", type=str,
+                        help="Path to output Doc2Vec model")
+    args = parser.parse_args()
+
+    if args.output:
+        output_path = args.output
+        if not output_path.endswith(".model"):
+            output_path = output_path + ".model"
+    else:
+        output_path = "hybrid_d2v.model"
+
+    pmid, join_text = load_tokens(args.input)
+    tagged_data = generate_TaggedDocument(pmid, join_text)
+    
+    params_d2v = {
+        "vector_size": 200, 
+        "window": 5, 
+        "min_count": 1, 
+        "epochs": 5, 
+        "workers": 4}
+
+    model = generate_doc2vec_model(tagged_data, params_d2v)
+    train_doc2vec_model(model, tagged_data, time_train = True)
+    save_doc2vec_model(model, output_path)
+    
+
+
+# def create_sim(model):
+#     output_file = "relish_relevance_matrix_full.csv"
+#     matrix_df = pd.read_csv("relish_relevance_matrix.csv")
+#     matrix_df["Cosine Similarity"] = ""
+
+#     for index, row in matrix_df.iterrows():
+#         ref_pmid = row["PMID Reference"]
+#         assessed_pmid = row["PMID Assessed"]
+
+#         try:
+#             row["Cosine Similarity"] = round(
+#                 model.docvecs.similarity(str(ref_pmid), str(assessed_pmid)), 2)
+#         except:
+#             row["Cosine Similarity"] = ""
+
+#         matrix_df.at[index, 'Cosine Similarity'] = row['Cosine Similarity']
+#     matrix_df.to_csv(output_file, index=False)
+
+
+# input_path = "../../../data_full/RELISH_tokens.npy"
+# pmid, join_text = load_tokens(input_path)
+# tagged_data = generate_TaggedDocument(pmid, join_text)
+# model = generate_doc2vec_model(tagged_data)
+# train_doc2vec_model(model, tagged_data)
+
+# create_sim(model)
+# model.save("train_inside.model")
---
+++ code/embeddings/preprocess.py
@@ -0,0 +1,153 @@
+import sys
+
+import argparse
+import logging
+
+import re
+import numpy as np
+import pandas as pd
+from string import punctuation
+
+
+def read_data(input_path: str) -> pd.DataFrame:
+    """
+    Reads the input .TSV files
+
+    Parameters
+    ----------
+    input_path : str
+        Input path for the .TSV files.
+
+    Returns
+    -------
+    pd.DataFrame
+        DataFrame with three columns: PMID, title and abstract
+    """
+    return pd.read_csv(input_path, delimiter="\t", quotechar="`")
+
+
+def preprocess_text(text: str, process_abv: bool = False) -> str:
+    """
+    Preprocess a given string. The steps are: 
+        1. Convert abbreviations such as "E. Coli" to a single word.
+        2. Set all the characters to lowercase.
+        3. Remove the desired punctuations from text.
+
+    Parameters
+    ----------
+    text : str
+        Text to be preprocessed. It must a be a single string.
+    process_abv : bool, optional
+        Whether modify abbreviations such as "E. coli", where it is transformed
+        as ecoli, by default False.
+
+    Returns
+    -------
+    text: str
+        The preprocecessed text file
+    """
+    if process_abv:
+        text = re.sub(abv_pattern, r"\1\3", text)
+
+    text_list = []
+    for word in text.lower().split():
+        word_list = []
+        for character in word:
+            # print(character)
+            if re.match(alphanumeric_pattern, character) or character in allowed_punctuation:
+                word_list.append(character)
+        word = "".join(word_list)
+        text_list.append(word)
+    text = " ".join(text_list).strip()
+
+    return text
+
+
+def preprocess_data(data: pd.DataFrame, process_abv: bool = False) -> pd.DataFrame:
+    """
+    Main pipeline to read the input DataFrame and preprocess both the title and
+    abstract columns.
+
+    Parameters
+    ----------
+    data : pd.DataFrame
+        Dataframe with three columns: PMID, title and abstract
+    process_abv : bool, optional
+        Whether modify abbreviations such as "E. coli", where it is transformed
+        as ecoli, by default False.
+
+    Returns
+    -------
+    pd.DataFrame
+        Preprocessed DataFrame with three columns: PMID, title and abstract
+    """
+    for i, row in data.iterrows():
+        title = row["title"]
+        abstract = row["abstract"]
+        #title = "MeSHD003643 signal-induced localization of p53 MeSHD011506 to MeSHD008928. A potential MeSHD012380 in apoptotic signaling. The element's H(2)O that we've described. The study of E. coli bacteria in E.coli sickness."
+
+        title = preprocess_text(title, process_abv)
+        abstract = preprocess_text(abstract, process_abv)
+        # print(title)
+
+        data.at[i, "title"] = title
+        data.at[i, "abstract"] = abstract
+
+    return(data)
+
+
+def save_output(data: pd.DataFrame, output_path: str, npy_format: bool = False) -> None:
+    """
+    Saves the dataframe into a .tsv file
+
+    Parameters
+    ----------
+    data : pd.DataFrame
+        Preprocessed dataframe to be saved.
+    output_path : str
+        Path for output file.
+    """
+    if npy_format:
+        if output_path.endswith(".tsv"):
+            output_path = output_path.replace(".tsv", ".npy")
+        output_data = []
+        for i, row in data.iterrows():
+            output_data.append([np.asanyarray(data.at[i, "PMID"]), 
+                np.asanyarray(data.at[i, "title"].split(), dtype=object), 
+                np.asanyarray(data.at[i, "abstract"].split(), dtype=object)])
+
+        np.save(output_path, np.asanyarray(output_data, dtype = object))
+    else:
+        data.to_csv(output_path, sep="\t", index=False, quotechar="`")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    #group = parser.add_mutually_exclusive_group(required=True)
+    parser.add_argument("-i", "--input", type=str,
+                        help="Path to input TSV file", required=True)
+    parser.add_argument("-o", "--output", type=str,
+                        help="Path to output TSV file")
+    args = parser.parse_args()
+
+    if not args.input.endswith(".tsv"):
+        logging.error(
+            f"The input file must be a .tsv, or tab separated values", exc_info=False)
+        sys.exit("No valid input file.")
+
+    if not args.output:
+        output_path = args.input.replace(".tsv", "_preprocessed.tsv")
+        logging.warning(
+            f"No output path specified, defaulting to {output_path}")
+    else:
+        output_path = args.output
+
+    abv_pattern = r"((?:^|\s)[a-zA-Z]\.)(\s?)([a-z]\w+)"
+    alphanumeric_pattern = r"[a-zA-Z\d]"
+    #alphanumeric_pattern = r".*[a-zA-Z\d\-].*"
+    allowed_punctuation = ["-"]
+    
+    data = read_data(args.input)
+    data = preprocess_data(data)
+    save_output(data, output_path, npy_format=False)
+    save_output(data, output_path, npy_format=True)
